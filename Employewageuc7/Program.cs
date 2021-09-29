9b42c50ab680e495a6a6c0e6dab2be76c5b56ca7
varnikak00 <91057679+varnikak00@users.noreply.github.com>
2021-09-29 12:57:53 +0530

    Update Program.cs

--- Employewageuc7/Program.cs
+++ Employewageuc7/Program.cs
@@ -1,42 +1,54 @@
-﻿using System;
+using System;
 
-namespace Employewageuc7
+namespace EmployeeWageUC7
 {
     class Program
     {
-       public static void Main(string[] args)
-        { }
-        public string company;
-        public int empRatePerHour;
-        public int numOfWorkingDays;
-        public int maxHoursPerMonths;
-        public int salary;
-        public Employewageuc7(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonths)
-        { 
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonths = maxHoursPerMonths;
+
+        public const int PART_TIME = 1;
+        public const int FULL_TIME = 2;
+        public const int Emp_Wage_Per_Hour = 20;
+        public const int Num_Of_Working_Days = 20; 
+        public const int Max_Hours_Per_Month = 100;
+    public static int ComputeEmpWage()
+    {
+        int EmpHours = 0, Totalworkhrs = 0, totalworkingdays = 0;
+        while (Totalworkhrs <= Max_Hours_Per_Month && totalworkingdays < Num_Of_Working_Days)
+        {
+            Random ran = new Random();
+            int Empcheck = ran.Next(0, 3);
+            switch (Empcheck)
+            {
+                case 1:
+                    Console.WriteLine("PART TIME");
+                    EmpHours = 4;
+                    break;
+
+                case 2:
+                    Console.WriteLine("FULL TIME");
+                    EmpHours = 8;
+                    break;
+                default:
+                    Console.WriteLine("ABSENT");
+                    EmpHours = 0;
+                    break;
+
+            }
+            totalworkingdays++;
+            Totalworkhrs += EmpHours;
+            Console.WriteLine("Day#:" + totalworkingdays + "Emp Hrs :" + EmpHours);
         }
 
-        public Program(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonths, int salary)
-        {
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonths = maxHoursPerMonths;
-            this.salary = salary;
-        }
+        int  TotalEmpwage =Totalworkhrs * Emp_Wage_Per_Hour;
+        Console.WriteLine("TOTAL WAGES =" + TotalEmpwage);
+        return TotalEmpwage;
 
-        public void setSalary(int salary)
-        {
-            this.salary = salary;
-        }
-        public string displaySalary()
-        {
-            return "total Emp wage for company: " + company + " is " + salary;
+    }
 
 
+        static void Main(string[] args)
+        {
+            ComputeEmpWage();
         }
     }
 }
